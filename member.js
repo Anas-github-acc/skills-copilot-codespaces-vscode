cafae7c004cc75125ad09ea5f1219cff2a32d04f
MdAnas <144697531+Anas-github-acc@users.noreply.github.com>
2024-05-31 19:53:23 +0000

    creating member.js

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMemeber() {
+    return {
+        name: 'John Doe',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        greet: function(){
+            console.log('Hello');
+        }
+    }
+}
